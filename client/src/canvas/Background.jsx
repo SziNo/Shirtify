0a3bbc7c20a6d81cff580db38501abe868cd32e5
SziNo <szino1848@gmail.com>
2023-05-03 22:14:48 +0200

    create Background component

---
+++ client/src/canvas/Background.jsx
@@ -0,0 +1,14 @@
+import { useLoader } from '@react-three/fiber'
+import { TextureLoader } from 'three'
+import { Plane } from '@react-three/drei'
+
+const Background = ({ url }) => {
+  const texture = useLoader(TextureLoader, url)
+  return (
+    <Plane args={[16, 9]} position={[0, 0, -10]}>
+      <meshBasicMaterial map={texture} />
+    </Plane>
+  )
+}
+
+export default Background
